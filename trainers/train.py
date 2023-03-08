51b509115216ed141c48bf9f6ba0c6936ae4b6e0
David Xiong <dxiong2001@g.ucla.edu>
2023-03-07 17:50:55 -0800

    Milestone 3 in progress

--- trainers/train.py
+++ trainers/train.py
@@ -218,8 +218,11 @@ def train(args, train_dataset, model, tokenizer):
             # (2) Compute the loss (store as `loss` variable)
             # Hint: See the HuggingFace transformers doc to properly get
             # the loss from the model outputs.
-            raise NotImplementedError("Please finish the TODO!")
-
+            output = model(inputs["input_ids"], 
+                    token_type_ids=inputs["token_type_ids"], 
+                    attention_mask=inputs["attention_mask"], 
+                    labels=inputs["labels"])
+            loss = output[0]
             if args.n_gpu > 1:
                 # Applies mean() to average on multi-gpu parallel training.
                 loss = loss.mean()
@@ -230,7 +233,8 @@ def train(args, train_dataset, model, tokenizer):
                 loss = loss / args.gradient_accumulation_steps
 
             # (3) Implement the backward for loss propagation
-            raise NotImplementedError("Please finish the TODO!")
+            
+            loss.backward()
 
             # End of TODO.
             ##################################################
@@ -377,7 +381,12 @@ def evaluate(args, model, tokenizer, prefix="", data_split="test"):
             ##################################################
             # TODO: Evaluation Loop
             # (1) Run forward and get the model outputs
-            raise NotImplementedError("Please finish the TODO!")
+            
+            outputs = model(inputs["input_ids"], 
+                    token_type_ids=inputs["token_type_ids"], 
+                    attention_mask=inputs["attention_mask"], 
+                    labels=inputs["labels"])
+            # raise NotImplementedError("Please finish the TODO!")
 
             if has_label or args.training_phase == "pretrain":
                 # (2) If label present or pretraining, compute the loss and prediction logits
@@ -387,7 +396,8 @@ def evaluate(args, model, tokenizer, prefix="", data_split="test"):
                 # indexing properly the outputs as tuples.
                 # Make sure to perform a `.mean()` on the eval loss and add it
                 # to the `eval_loss` variable.
-                raise NotImplementedError("Please finish the TODO!")
+                loss = outputs[0]
+                # raise NotImplementedError("Please finish the TODO!")
             else:
                 # (3) If labels not present, only compute the prediction logits
                 # Label the logits as `logits`
