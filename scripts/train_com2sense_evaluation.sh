3657dd72f02494350b4c86254ba8407ac38c43af
edmondywen <edmondywen@gmail.com>
2023-03-15 10:32:34 -0700

    edit train_com2sense_eval

--- scripts/train_com2sense_evaluation.sh
+++ scripts/train_com2sense_evaluation.sh
@@ -1,11 +1,11 @@
 TASK_NAME="com2sense"
 DATA_DIR="datasets/com2sense"
 MODEL_TYPE="bert-base-cased"
-# MODEL_PATH="/Users/edmond/Documents/cs162/winter23_cs162_course_project_student/outputs/com2sense/ckpts/checkpoint-380/pytorch_model.bin"
+MODEL_PATH="/Users/edmond/Documents/cs162/winter23_cs162_course_project_student/outputs/com2sense/ckpts/checkpoint-380/pytorch_model.bin"
 
 
 python3 -m trainers.train \
-  --model_name_or_path ${MODEL_TYPE} \
+  --model_name_or_path ${MODEL_PATH} \
   --do_not_load_optimizer \
   --do_eval \
   --per_gpu_train_batch_size 4 \
@@ -22,7 +22,7 @@ python3 -m trainers.train \
   --warmup_steps 100 \
   --eval_split "test" \
   --score_average_method "binary" \
-  --iters_to_eval 20 40 \
+  --iters_to_eval 20 40 4000 5000 \
   --overwrite_output_dir \
   # --max_eval_steps 1000 \
   # --evaluate_during_train
